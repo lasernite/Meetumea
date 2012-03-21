55e46f4bcabd59a22cd719da1e491afb78dbedcf
Laser Nite <lasernite@Lasers-MacBook-Pro.local>
2012-03-21 11:08:25 -0500

    Sign-in-out complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Meetume, your online community!"
       redirect_to @user
     else
