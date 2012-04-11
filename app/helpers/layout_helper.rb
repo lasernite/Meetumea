6fb6af2cc6891d8fd29db7a102bcc7c4c398d858
Laser Nite <lasernite@Lasers-MacBook-Pro.local>
2012-04-11 14:23:34 -0500

    location displayed from partial

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -19,4 +19,9 @@ module LayoutHelper
   def javascript(*args)
     content_for(:head) { javascript_include_tag(*args) }
   end
+  
+  def location
+    @city = request.location.city
+    @state = request.location.state
+  end
 end
