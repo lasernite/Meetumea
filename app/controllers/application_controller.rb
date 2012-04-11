e065fb48184d37d003d727114963820828620f44
Laser Nite <lasernite@Lasers-MacBook-Pro.local>
2012-04-11 15:23:19 -0500

    fixed mabe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-  before_filter :location
-  
-  def location
-    @city = request.location.city
-    @state = request.location.state
-  end
 end
